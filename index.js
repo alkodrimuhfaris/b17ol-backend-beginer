61e525e63182f61c7c2028e46f5f39aad2e27e2b
alkodrimuhfaris <63344061+alkodrimuhfaris@users.noreply.github.com>
2020-09-09 06:50:03 +0700

    Penambahan soal nomor 3, pengiriman input dari user di page data.ejs ke page /data

--- index.js
+++ index.js
@@ -11,20 +11,30 @@ const { send } = require("process");
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+//nomor 1
 app.get('/home', (req, res)=>{
     res.send('Welcome to my first backend!')
 })
 
+//nomor 2
 app.get("/main", (req,res)=>{
     res.render(path.join(__dirname, 'data.ejs'));
 })
 
+//nomor 2
+// app.post('/data', (req, res)=>{
+//     let name = req.body.data;
+//     console.log("post :", name);
+//     res.sendStatus(200);
+// })
+
+
+//nomor 3
 app.post('/data', (req, res)=>{
     let name = req.body.data;
-    console.log("post :", name);
-    res.sendStatus(200);
+    //console.log("post :", name);
+    res.send(name);
 })
-
 
 
 app.listen(3000,()=>{
